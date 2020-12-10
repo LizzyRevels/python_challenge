1ab7fe8e7d64298dd5e5457e1d6b065a3d1d3285
Lizzy Revels <lizzyrevels1@gmail.com>
2020-12-10 01:05:16 -0500

    Adding updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,13 @@
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
 import os
-
-# Module for reading CSV files
 import csv
 
 # Specify the file to write to
-output_path = os.path.join("Resources", "output", "output.csv")
+output_path = os.path.join("Resources", "output", "output.txt")
 
 # Path to collect data from the Resources folder
 csvpath = os.path.join('Resources','budget_data.csv')
 
-# save the output file path
+# Save the output file path
 output_file = os.path.join("output.txt")
 
 # open the output file
@@ -23,34 +19,34 @@ output_file = os.path.join("output.txt")
 #Read data
 with open(csvpath) as csvfile:
 
-    # CSV reader specifies delimiter and variable that holds contents
+# CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-
-    # # Read the header row first (skip this step if there is now header)
+ # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    # print(f"CSV Header: {csv_header}")
 
-   
+# Create a Python script that analyzes the records:
     counter=0
     sum=0
     list_one=[]
     
-
  # Read each row of data after the header
     for row in csvreader:
+        
+        # The total number of months included in the dataset
         counter=counter+1
-        list_one.append(row [1])
+        list_one.append(row[1])
+
+        # The net total amount of "Profit/Losses" over the entire period
         sum+=int(row[1])
         
     print(counter)
     print(list_one)
     print(sum)
-    print(sum/counter)
+    # print(sum/counter)
 
+    # The greatest increase in profits (date and amount) over the entire period
     length_list=len(list_one)
-
-
     list_two=[]
     max_increase=0
     max_decrease=0
@@ -63,10 +59,12 @@ with open(csvpath) as csvfile:
 
     print(max_increase)
             
-   for m in range(1, length_list):
+    for m in range(1, length_list):
         difference=(int(list_one[m])-int(list_one[m-1]))
         list_two.append(difference)
         if difference < max_decrease: 
             max_increase=difference
+
+    print(max_decrease)
 
 
