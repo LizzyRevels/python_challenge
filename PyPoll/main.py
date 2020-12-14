c89b3d9d618fc813708bf054e729a25c835b5c8d
Lizzy Revels <lizzyrevels1@gmail.com>
2020-12-14 15:57:57 -0500

    adding changes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,7 +7,6 @@ csvpath = os.path.join('Resources','election_data.csv')
 # Save the output file path
 output_file = os.path.join("analysis.txt")
 
-
 # Read data
 with open(csvpath) as csvfile:
 
@@ -26,54 +25,48 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         counter=counter+1
         vote_counts.append(row[2])
-        
 
-# A complete list of candidates who received votes
-    # print(vote_counts)
-    Khan_votes = vote_counts.count("Khan")
-    tota_votes = len(vote_counts)
-    Khan_percent = (Khan_votes/tota_votes) * 100
-    print(Khan_percent) 
-    print(Khan_votes)
+    total_votes = len(vote_counts)   
+    print(counter)
     candidates = set(vote_counts)
     print(candidates)
 
-    # Khan = int()
-    # Correy = int()
-    # Li = int()
-    # OTooley = int()
-    # for x in csvreader:
-    #     candidates.append(x[2])
-    #     if x[2] == "Khan":
-    #         Khan += 1
-    #     if x[2] == "Correy":
-    #         Correy += 1
-    #     if x[2] == "Li":
-    #         Li += 1
-    #     if x[2] == "O'Tooley":
-    #         OTooley += 1
-        
-
-# The percentage of votes each candidate won
-
-    # candidates_set = (set(candidates))
-    # Khan_percent = float(Khan/counter) * 100
-    # Correy_percent = float(Correy/counter) 
-    # Li_percent = float(Li/counter) * 100
-    # OTooley_percent = float(OTooley/counter) * 100
-
-    print(counter)
+# A complete list of candidates who received votes
+    Khan_votes = vote_counts.count("Khan")
+    Khan_percent = (Khan_votes/total_votes) * 100
+    print(Khan_percent) 
+    print(Khan_votes)
     
+    Correy_votes = vote_counts.count("Correy")
+    Correy_percent = (Correy_votes/total_votes) * 100
+    print(Correy_percent) 
+    print(Correy_votes)
+
+    Li_votes = vote_counts.count("Li")
+    Li_percent = (Li_votes/total_votes) * 100
+    print(Li_percent) 
+    print(Li_votes)
+
+    OTooley_votes = vote_counts.count("O'Tooley")
+    OTooley_percent = (OTooley_votes/total_votes) * 100
+    print(OTooley_percent) 
+    print(OTooley_votes)  
+    
+    # The winner of the election based on popular vote.
+    max_votes=max({Khan_votes} , {Correy_votes} , {Li_percent} , {OTooley_votes})
+    print(max_votes)
 
 # export to text file
 with open(output_file, "w", newline='') as textfile:
     textfile.write("Election Results\n")
     textfile.write("-------------------\n")
-    textfile.write(f'Total Votes:{counter}\n')
+    textfile.write(f'Total Votes: {counter}\n')
     textfile.write("-------------------\n")
-    # textfile.write(f'Khan:{Khan_percent}\n')
-    # textfile.write(f'Correy:{Correy_percent}\n')
-    # textfile.write(f'Li:{Li_percent}\n')
-    # textfile.write(f'OTooley:{OTooley_percent}\n')
+    textfile.write(f'Khan: {round(Khan_percent)}% {Khan_votes}\n')
+    textfile.write(f'Correy: {round(Correy_percent)}% {Correy_votes}\n')
+    textfile.write(f'Li: {round(Li_percent)}% {Li_votes}\n')
+    textfile.write(f'O\'Tooley: {round(OTooley_percent)}% {OTooley_votes}\n')
+    textfile.write("-------------------\n")
+    textfile.write(f'Winner: Khan')
     # textfile.write(f'Winner':{x})
    
