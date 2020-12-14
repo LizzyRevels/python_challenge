62b2d920eed2b8e6589223740bfc169b13399220
Lizzy Revels <lizzyrevels1@gmail.com>
2020-12-14 12:29:34 -0500

    adding updates with tutor

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,8 +7,6 @@ csvpath = os.path.join('Resources','election_data.csv')
 # Save the output file path
 output_file = os.path.join("analysis.txt")
 
-# Path to output
-output_file =os.path.join("analysis.txt")
 
 # Read data
 with open(csvpath) as csvfile:
@@ -21,59 +19,61 @@ with open(csvpath) as csvfile:
 
 # Create a Python script that analyzes the records:
     counter=0
-    sum=0
+    vote_counts=[]
     
 
 # The total number of votes included in the dataset
     for row in csvreader:
         counter=counter+1
+        vote_counts.append(row[2])
         
 
 # A complete list of candidates who received votes
-    # I know I need to iterate through the names in the Candidate column (Index 2) and 
-    # count each occurrence of each unique name. Do I need to use if/elif/else? 
-        # JUST REALIZED -- The names are listed on the Homework instructions, so I could use 
-        # those names as a starting off point. I was looking through my classmates examples
-        # and not seeing their code on where they found the names. All the while the names
-        # are already given.
+    # print(vote_counts)
+    Khan_votes = vote_counts.count("Khan")
+    tota_votes = len(vote_counts)
+    Khan_percent = (Khan_votes/tota_votes) * 100
+    print(Khan_percent) 
+    print(Khan_votes)
+    candidates = set(vote_counts)
+    print(candidates)
 
-        candidates = []
-        Khan = int()
-        Correy = int()
-        Li = int()
-        OTooley = int()
-        for x in csvreader:
-            candidates.append(x[2])
-            if x[2] == "Khan":
-                Khan += 1
-            if x[2] == "Correy":
-                Correy += 1
-            if x[2] == "Li":
-                Li += 1
-            if x[2] == "O'Tooley":
-                OTooley += 1
-
+    # Khan = int()
+    # Correy = int()
+    # Li = int()
+    # OTooley = int()
+    # for x in csvreader:
+    #     candidates.append(x[2])
+    #     if x[2] == "Khan":
+    #         Khan += 1
+    #     if x[2] == "Correy":
+    #         Correy += 1
+    #     if x[2] == "Li":
+    #         Li += 1
+    #     if x[2] == "O'Tooley":
+    #         OTooley += 1
+        
 
 # The percentage of votes each candidate won
 
-    candidates_set = (set(candidates))
-    Khan_percent = (Khan/counter) * 100
-    Correy_percent = float(Correy/counter) 
-    Li_percent = float(Li/counter) * 100
-    OTooley_percent = float(OTooley/counter) * 100
+    # candidates_set = (set(candidates))
+    # Khan_percent = float(Khan/counter) * 100
+    # Correy_percent = float(Correy/counter) 
+    # Li_percent = float(Li/counter) * 100
+    # OTooley_percent = float(OTooley/counter) * 100
 
+    print(counter)
+    
 
-# print(counter)
-# print(candidates)
-
-# #export to text file
+# export to text file
 with open(output_file, "w", newline='') as textfile:
     textfile.write("Election Results\n")
     textfile.write("-------------------\n")
     textfile.write(f'Total Votes:{counter}\n')
     textfile.write("-------------------\n")
-    textfile.write(f'Khan:{Khan_percent}\n')
-    textfile.write(f'Correy:{Correy_percent}\n')
-    textfile.write(f'Li:{Li_percent}\n')
-    textfile.write(f'OTooley:{OTooley_percent}\n')
+    # textfile.write(f'Khan:{Khan_percent}\n')
+    # textfile.write(f'Correy:{Correy_percent}\n')
+    # textfile.write(f'Li:{Li_percent}\n')
+    # textfile.write(f'OTooley:{OTooley_percent}\n')
+    # textfile.write(f'Winner':{x})
    
